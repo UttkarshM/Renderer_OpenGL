27f3658e208a7a8f4172c2051940e266053169b9
Uttkarsh M <109910700+UttkarshM@users.noreply.github.com>
2024-01-21 16:59:44 +0530

    Delete build directory

--- build/external/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/uttkarsh/OpenGL_Library/OpenGL_library/external/glm
-# Build directory: /home/uttkarsh/OpenGL_Library/OpenGL_library/build/external/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
--- build/external/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/uttkarsh/OpenGL_Library/OpenGL_library/external/glm/glm
-# Build directory: /home/uttkarsh/OpenGL_Library/OpenGL_library/build/external/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
